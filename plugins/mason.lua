0182c093b5c7f14bf636e21254f437ea4c38ef1e
vynubadri <vynu@live.com>
2025-11-14 13:10:24 -0600

    Replace lua_ls with luau_lsp and add custom handlers

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -17,7 +17,7 @@ return {
 
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "lua_ls",        -- Lua
+        "luau_lsp",      -- Lua/Luau
         "ts_ls",         -- TypeScript/JavaScript
         "pyright",       -- Python
         "gopls",         -- Go
@@ -29,6 +29,123 @@ return {
         "tailwindcss",   -- Tailwind
       },
       automatic_installation = true,
+      
+      -- Automatically setup LSP servers installed via Mason
+      handlers = {
+        -- Default handler for all servers
+        function(server_name)
+          vim.lsp.enable(server_name)
+        end,
+        
+        -- Custom handler for luau_lsp
+        ["luau_lsp"] = function()
+          vim.lsp.config.luau_lsp = {
+            settings = {
+              luau = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+              },
+            },
+          }
+          vim.lsp.enable("luau_lsp")
+        end,
+
+        -- Custom handler for pyright
+        ["pyright"] = function()
+          vim.lsp.config.pyright = {
+            settings = {
+              python = {
+                analysis = {
+                  typeCheckingMode = "basic",
+                  autoSearchPaths = true,
+                  useLibraryCodeForTypes = true,
+                },
+              },
+            },
+          }
+          vim.lsp.enable("pyright")
+        end,
+
+        -- Custom handler for gopls
+        ["gopls"] = function()
+          vim.lsp.config.gopls = {
+            settings = {
+              gopls = {
+                analyses = {
+                  unusedparams = true,
+                },
+                staticcheck = true,
+              },
+            },
+          }
+          vim.lsp.enable("gopls")
+        end,
+
+        -- Custom handler for rust_analyzer
+        ["rust_analyzer"] = function()
+          vim.lsp.config.rust_analyzer = {
+            settings = {
+              ["rust-analyzer"] = {
+                cargo = {
+                  allFeatures = true,
+                },
+              },
+            },
+          }
+          vim.lsp.enable("rust_analyzer")
+        end,
+
+        -- Custom handler for yamlls
+        ["yamlls"] = function()
+          vim.lsp.config.yamlls = {
+            settings = {
+              yaml = {
+                schemas = {
+                  ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+                  ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.yml",
+                  kubernetes = "/*.yaml",
+                },
+                format = {
+                  enable = true,
+                },
+              },
+            },
+          }
+          vim.lsp.enable("yamlls")
+        end,
+
+        -- Custom handler for ts_ls
+        ["ts_ls"] = function()
+          vim.lsp.config.ts_ls = {
+            settings = {
+              typescript = {
+                inlayHints = {
+                  includeInlayParameterNameHints = "all",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = true,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
+                },
+              },
+              javascript = {
+                inlayHints = {
+                  includeInlayParameterNameHints = "all",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = true,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
+                },
+              },
+            },
+          }
+          vim.lsp.enable("ts_ls")
+        end,
+      },
     })
 
     require("mason-tool-installer").setup({
