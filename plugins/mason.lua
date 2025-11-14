219dab19b53481e5dc76cda5b62c4e45cfe281b2
vynubadri <vynu@live.com>
2025-11-14 10:58:09 -0600

    Initialize mason.nvim with LSP configurations
    Add configuration for mason.nvim and its dependencies

---
+++ plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    require("mason").setup()
+    
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "tsserver",
+        "pyright",
+        "gopls",
+        "rust_analyzer",
+      },
+      automatic_installation = true,
+    })
+  end,
+}
