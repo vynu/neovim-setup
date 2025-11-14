7255fef18fc17e17847703215f4ff20f60ed1be3
vynubadri <vynu@live.com>
2025-11-14 12:09:19 -0600

    Add nvim-cmp configuration for Neovim

---
+++ plugins/nvim-cmp.lua
@@ -0,0 +1,77 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",           -- Buffer completions
+    "hrsh7th/cmp-path",             -- Path completions
+    "hrsh7th/cmp-nvim-lsp",         -- LSP completions
+    "hrsh7th/cmp-nvim-lua",         -- Neovim Lua API completions
+    "saadparwaiz1/cmp_luasnip",     -- Snippet completions
+    "L3MON4D3/LuaSnip",             -- Snippet engine
+    "rafamadriz/friendly-snippets", -- Collection of snippets
+    "onsails/lspkind.nvim",         -- VS Code-like pictograms
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+
+    -- Load VS Code-style snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- Previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- Next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- Show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- Close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm selection
+        -- Tab through suggestions
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        -- Shift-Tab for previous
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "nvim_lua" },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          maxwidth = 50,
+          ellipsis_char = "...",
+        }),
+      },
+    })
+  end,
+}
