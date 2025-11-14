e21864d4f7561a5ddb9e4d52c162786aba7b2180
vynubadri <vynu@live.com>
2025-11-14 10:57:46 -0600

    Add nvim-treesitter configuration

---
+++ plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua", "vim", "vimdoc",
+        "javascript", "typescript", "tsx",
+        "python", "go", "rust",
+        "html", "css", "json", "yaml",
+        "markdown", "bash",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    })
+  end,
+}
