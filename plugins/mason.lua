3dce57c893249a96839cb2cf1624a08180212d0d
vynubadri <vynu@live.com>
2025-11-14 12:08:05 -0600

    Update mason.nvim setup with icons and tools
    Enhance mason.nvim configuration with additional tools and icons.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -2,19 +2,47 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    require("mason").setup()
-    
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "pyright",
-        "gopls",
-        "rust_analyzer",
+        "lua_ls",        -- Lua
+        "ts_ls",         -- TypeScript/JavaScript
+        "pyright",       -- Python
+        "gopls",         -- Go
+        "rust_analyzer", -- Rust
+        "yamlls",        -- YAML
+        "jsonls",        -- JSON
+        "html",          -- HTML
+        "cssls",         -- CSS
+        "tailwindcss",   -- Tailwind
       },
       automatic_installation = true,
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettier",    -- Formatter
+        "stylua",      -- Lua formatter
+        "black",       -- Python formatter
+        "isort",       -- Python import formatter
+        "gofmt",       -- Go formatter
+        "goimports",   -- Go imports
+        "rustfmt",     -- Rust formatter
+        "eslint_d",    -- JS linter
+        "pylint",      -- Python linter
+      },
     })
   end,
 }
